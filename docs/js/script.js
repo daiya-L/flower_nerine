0e5d2386c6247ba7f933df963623102b6cd3e09a
daiya-elu <tenbeyjun@icloud.com>
2019-10-17 23:08:57 +0900

    update

---
+++ docs/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 1000;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
+$(function() {
+  $(".nav-toggle").click(function() {
+    $(".nav-toggle div").toggleClass("active");
+  });
+});
